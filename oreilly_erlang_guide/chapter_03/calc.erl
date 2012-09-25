d3f592c902ea1f72166f5216e787abe394995326
boostbob <1982wb@gmail.com>
2012-09-25 14:14:33 +0800

    save

--- oreilly_erlang_guide/chapter_03/calc.erl
+++ oreilly_erlang_guide/chapter_03/calc.erl
@@ -4,15 +4,13 @@
 -export([sum/1, sum/2]).
 
 %%
-sum(0) -> 
-  0;
+sum(0) -> 0;
 
 sum(N) when N > 0 -> 
   N + sum(N - 1).
 
 %%
-sum(N, M) when N > M ->
-  0;
+sum(N, M) when N > M -> 0;
 
 sum(N, M) when (N =< M) -> 
   N + sum(N + 1, M).
