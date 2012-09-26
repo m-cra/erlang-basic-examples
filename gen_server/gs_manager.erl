5eff2c7c2cbe7f4be2c369715f99207aef609f72
boostbob <1982wb@gmail.com>
2012-09-26 14:53:03 +0800

    save

--- gen_server/gs_manager.erl
+++ gen_server/gs_manager.erl
@@ -1,4 +1,4 @@
-%% gs_impl.erl 
+%% gs manager 
 
 -module(gs_manager).
 -behaviour(gen_server).
@@ -13,25 +13,32 @@
 
 -record(state, {}).
 
+%% 服务器启动函数
 start_link() ->
   gen_server:start_link({local, manager}, ?MODULE, [], []).
 
+%% 启动
 init([]) ->
   {ok, #state{}}.
 
+%% 同步调用
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
 
+%% 异步调用
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
+%% 处理原生消息
 handle_info(Info, State) ->
   io:format("msg received: ~n", []),
-  io:format("input msg is: ~w~n", [Info]),
+  io:format("input msg: ~w~n", [Info]),
   {noreply, State}.
 
+%% 终结
 terminate(_Reason, _State) ->
   ok.
 
+%% 动态部署
 code_change(_Old, State, _Ext) ->
   {ok, State}.
--- gen_server/gs_server.erl
+++ gen_server/gs_server.erl
@@ -4,15 +4,18 @@
 -export([start/0]).
 
 start()->
-  %%监听端口
-  {ok, ListenSocket} = gen_tcp:listen(1234,[binary,{packet,4},  
-                                          {reuseaddr,true},  
-                                          {active,true}]),
-  %%获取客户端链接  
+  %% 监听端口
+  {ok, ListenSocket} = gen_tcp:listen(9927,[binary,{packet,4},  
+                                           {reuseaddr,true},  
+                                           {active,true}]),
+  %% 获取客户端链接  
   {ok, Socket} = gen_tcp:accept(ListenSocket),
-  %%关闭监听  
+
+  %% 关闭监听  
   gen_tcp:close(ListenSocket),
-  %%启动一个gen_server  
-  {ok, Pid} = socketManager:start_link(),
-  %%将客户端链接socket 绑定到gen_server 
+
+  %% 启动一个gen_server  
+  {ok, Pid} = gs_manager:start_link(),
+
+  %% 将客户端链接socket 绑定到gen_server 
   gen_tcp:controlling_process(Socket, Pid).
