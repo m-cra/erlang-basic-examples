8d7f1f71c4e7968f81dccd2da3f6bf62d6e542c7
boostbob <1982wb@gmail.com>
2012-09-26 14:01:09 +0800

    save

---
+++ gen_server/gs_impl.erl
@@ -0,0 +1,38 @@
+%% gs_impl.erl 
+
+-module(gs_impl).
+-behaviour(gen_server).
+
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3,
+         start_link/0]).
+
+-record(state, {}).
+
+
+start_link()->
+  gen_server:start_link({local, socket_manager}, ?MODULE, [], []).
+
+init([])->
+  {ok, #state{}}.
+
+handle_call(_Request, _From, State)->
+  {reply, ok, State}.
+
+handle_cast(_Msg, State)->
+  {noreply, State}.
+
+handle_info(Info, State)->
+  io:format("msg received: ~n", []),
+  io:format("input msg is: ~w~n", [Info]),
+  {noreply, State}.
+
+terminate(_Reason, _State)->
+  ok.
+
+code_change(_Old, State,_Ext)->
+  {ok, State}.
