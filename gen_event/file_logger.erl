6563d847b5159bd9c86969e2fab3cfdbbb5c775d
boostbob <1982wb@gmail.com>
2012-09-26 20:42:40 +0800

    save

---
+++ gen_event/file_logger.erl
@@ -0,0 +1,16 @@
+%% file logger 
+
+-module(file_logger).
+-behaviour(gen_event).
+
+-export([init/1, handle_event/2, terminate/2]).
+
+init(File) ->
+  {ok, _Fd} = file:open(File, read).
+
+handle_event(ErrorMsg, Fd) ->
+  io:format(Fd, "***Error*** ~p~n", [ErrorMsg]),
+  {ok, Fd}.
+
+terminate(_Args, Fd) ->
+  file:close(Fd).
